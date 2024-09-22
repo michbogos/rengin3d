b3365d7ff46c9de95c632abe43ae5842898fdc7e
michbogos <bogostamicheal@gmail.com>
2024-09-22 20:26:44 +0200

    Add set method

--- src/draw.rs
+++ src/draw.rs
@@ -8,12 +8,28 @@ pub struct Surface{
 
 
 impl Surface{
-    fn new(width:usize, height:usize)->Surface{
+    pub fn new(width:usize, height:usize)->Surface{
         let buffer:Vec<char> = vec![' '; width*height];
         return Surface {width, height, buffer};
     }
 
-    fn clear(&mut self){
+    pub fn clear(&mut self){
         self.buffer = vec![' '; (self.width*self.height) as usize];
+    }
+
+    pub fn show(self){
+        for c in self.buffer {
+            print!("{}", c);
+        }
+    }
+
+    pub fn set(&mut self, x:usize, y:usize){
+        if x>=self.width{
+            panic!("Attempted set out of x range\n");
+        }
+        if y>=self.height{
+            panic!("Attempted set out of y range\n");
+        }
+        self.buffer[y*self.width+x] = '#';
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 mod draw;
 
 fn main() {
-    draw::clear();
+    let mut surface : draw::Surface = draw::Surface::new(165, 10);
+    surface.clear();
+    for i in 0..165{
+        for j in 0..10{
+            if (i+j) % 2 == 0{
+                surface.set(i, j);
+            }
+        }
+    }
+    surface.show();
 }
