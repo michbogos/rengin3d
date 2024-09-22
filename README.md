549e6f8bc874a424cd1d18d29659bfe756100a7e
michbogos <bogostamicheal@gmail.com>
2024-09-22 20:48:11 +0200

    Add readme and command line args.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rengin3d
+
+A rust 3d graphics library for the console.
+
+Run command:
+
+``` bash
+cargo run -- --width=$(tput cols) --height=$(tput lines)
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+use std::env;
+
 mod draw;
 
 fn main() {
-    let mut surface : draw::Surface = draw::Surface::new(165, 10);
+    let args:Vec<String> = env::args().collect();
+    let mut height:usize = 20;
+    let mut width:usize = 80;
+    for arg in &args{
+        if arg.contains("width"){
+            width = arg.split("=").last().unwrap().parse::<usize>().unwrap();
+        }
+        if arg.contains("height"){
+            height = arg.split("=").last().unwrap().parse::<usize>().unwrap();
+        }
+    }
+    let mut surface : draw::Surface = draw::Surface::new(width, height);
     surface.clear();
     for i in 0..165{
         for j in 0..10{
-            if (i+j) % 2 == 0{
+            if (i/4+j/4) % 2 == 0{
                 surface.set(i, j);
             }
         }
